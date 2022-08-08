56837174a1cf097d5cf516d09a0260bde524fa00
lfourg <limengnew@163.com>
2022-08-08 11:20:29 +0800

    change

---
+++ assets/noAxis.html.25fc17ee.js
@@ -0,0 +1,56 @@
+import{a as s,b as a,e as p,F as t,i as e}from"./app.8a8eb44e.js";import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";const r={},l=e(`<h1 id="\u975E\u5750\u6807\u8F74" tabindex="-1"><a class="header-anchor" href="#\u975E\u5750\u6807\u8F74" aria-hidden="true">#</a> \u975E\u5750\u6807\u8F74</h1><p>\u5305\u542B\u7684\u7EC4\u4EF6\u7C7B\u578B\u6709\uFF1A\u997C\u56FE\u3001\u65E5\u5386\u56FE\u3001\u77E9\u5F62\u5F62\u3001\u6C34\u7403\u56FE\u3001\u4EEA\u8868\u76D8\u3001\u8BCD\u4E91\u3001\u96F7\u8FBE\u56FE\u3001\u6C14\u6CE1\u56FE\u3001\u6811\u5F62\u56FE\u7B49\u3002</p><h4 id="\u6570\u636E\u683C\u5F0F" tabindex="-1"><a class="header-anchor" href="#\u6570\u636E\u683C\u5F0F" aria-hidden="true">#</a> \u6570\u636E\u683C\u5F0F</h4><div class="language-json ext-json line-numbers-mode"><pre class="language-json"><code><span class="token punctuation">[</span>
+ <span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u65B0\u6D6A\u5FAE\u535A&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">25</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u5FAE\u4FE1\u516C\u4F17\u53F7&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">38</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u65B0\u534E\u7F51&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">43</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u4EBA\u6C11\u7F51&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">65</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">]</span>
+</code></pre><div class="highlight-lines"><br><br><div class="highlight-line">\xA0</div><div class="highlight-line">\xA0</div><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br></div></div><p>\u6709\u4E9B\u7279\u6B8A\u60C5\u51B5\uFF0C\u53EF\u4EE5\u7075\u6D3B\u6269\u5C55\u6570\u636E\u5C5E\u6027\uFF0C\u6BD4\u5982\u96F7\u8FBE\u56FE\u6DFB\u52A0\u5C5E\u6027max\u3001s\uFF0C\u6811\u5F62\u56FE\u6DFB\u52A0\u5C5E\u6027children\u3002</p><div class="language-json ext-json line-numbers-mode"><pre class="language-json"><code><span class="token punctuation">[</span>
+  <span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u9500\u552E&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">4300</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;max&quot;</span><span class="token operator">:</span> <span class="token number">6500</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;s&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u9884\u7B97\u5206\u914D&quot;</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">{</span>
+    <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u9500\u552E&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">4300</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;max&quot;</span><span class="token operator">:</span> <span class="token number">6500</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;s&quot;</span><span class="token operator">:</span> <span class="token string">&quot;\u5B9E\u9645\u5F00\u9500&quot;</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+<span class="token punctuation">]</span>
+</code></pre><div class="highlight-lines"><br><br><div class="highlight-line">\xA0</div><div class="highlight-line">\xA0</div><div class="highlight-line">\xA0</div><div class="highlight-line">\xA0</div><br><br><br><br><br><br><br><br></div><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br></div></div><div class="language-json ext-json line-numbers-mode"><pre class="language-json"><code><span class="token punctuation">{</span>
+  <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;a&quot;</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;children&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">{</span>
+      <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;a-a&quot;</span><span class="token punctuation">,</span>
+      <span class="token property">&quot;children&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+          <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;a-a-a&quot;</span><span class="token punctuation">,</span>
+          <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">353</span>
+        <span class="token punctuation">}</span>
+      <span class="token punctuation">]</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;a-b&quot;</span><span class="token punctuation">,</span>
+      <span class="token property">&quot;children&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token punctuation">{</span>
+          <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;a-b-a&quot;</span><span class="token punctuation">,</span>
+          <span class="token property">&quot;value&quot;</span><span class="token operator">:</span> <span class="token number">17010</span>
+        <span class="token punctuation">}</span>
+      <span class="token punctuation">]</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">]</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br></div></div><h4 id="\u6837\u5F0F\u9762\u677F" tabindex="-1"><a class="header-anchor" href="#\u6837\u5F0F\u9762\u677F" aria-hidden="true">#</a> \u6837\u5F0F\u9762\u677F</h4><p>\u5305\u542B\u7684\u7EC4\u5982\u4E0B\u56FE\uFF0C\u5177\u4F53\u5404\u7EC4\u5305\u542B\u7684\u5C5E\u6027\u53EF\u53C2\u8003\u4EE3\u7801\u6837\u4F8B\u3002</p>`,9),c=["src"];function u(n,i){return s(),a(t,null,[l,p("img",{src:n.$withBase("/images/guide/standard/noAxisPanel.png"),style:{width:"300px","margin-top":"5px","border-radius":"5px"},alt:"\u6837\u5F0F\u9762\u677F"},null,8,c)],64)}var m=o(r,[["render",u]]);export{m as default};
